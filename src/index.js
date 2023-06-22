37396b7d209cef0d6bfa20a7b9718609b604d252
Peter <bachowskip@gmail.com>
2023-06-22 14:44:28 +0700

    Add watch script, create test module, re-create index.html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import _ from 'lodash';
 import './styles.css';
+import test from './test';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+test();
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,10 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    //clean: true,
   },
-  mode: 'development',
+  devtool: 'inline-source-map',
+  mode: 'development', 
   module: {
     rules: [
       {
@@ -20,6 +22,14 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
       },
     ],
   },
